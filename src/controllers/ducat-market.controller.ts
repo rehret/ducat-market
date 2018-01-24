f291de3124a92a6a06627615ee1edd56791048be
Randy Ehret <randyehret@gmail.com>
2018-01-24 16:53:13 -0600

    Make api routes more RESTful

--- src/controllers/ducat-market.controller.ts
+++ src/controllers/ducat-market.controller.ts
@@ -1,15 +1,26 @@
-import { Controller, Get } from 'routing-controllers';
+import { Controller, Get, Param } from 'routing-controllers';
 import { injectable } from 'inversify';
 import { Item } from '../models/item';
 import { DucatMarketService } from '../services/ducat-market.service';
+import { ItemProvider } from '../services/item.provider';
 
 @injectable()
 @Controller('/api')
 export class DucatMarketController {
 
-    @Get('/')
-    public async GetBestDeals(): Promise<Item[]> {
+    @Get('/items')
+    public async GetItems(): Promise<Item[]> {
+        return ItemProvider.GetItems();
+    }
+
+    @Get('/items/top')
+    public async GetTopItems(): Promise<Item[]> {
         return DucatMarketService.GetTopItems();
+    }
+
+    @Get('/items/top/:count')
+    public async GetTopXItems(@Param('count') count: number): Promise<Item[]> {
+        return DucatMarketService.GetTopItems(count);
     }
 
 }
